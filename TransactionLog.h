ba819fdc393b29ebfe2307c4aa7d2fb3e5d3d641
HaGiang58-cpp <a1901146@adelaide.edu.au>
2024-10-15 10:38:54 +1030

    add comment Transaction

--- TransactionLog.h
+++ TransactionLog.h
@@ -5,17 +5,39 @@
 
 using namespace std;
 
-// TransactionLog class declaration
+/**
+ * @brief Manages a log of transactions.
+ */
 class TransactionLog {
 private:
-    // Private member variable to store transactions
+    /**
+     * @brief Private member variable to store transactions
+     */
     vector<Transaction> transactions;
 
 public:
-    // Log a transaction to the log
+    /**
+     * @brief Logs a new transaction into the log.
+     * 
+     * @param transaction The transaction to log.
+     */
     void logTransaction(const Transaction& transaction);
 
-    // Display the transaction log
+    /**
+     * @brief Clears the transaction log.
+     */
+    void clear();  
+
+    /**
+     * @brief Displays the transaction log to the console.
+     */
     void displayLog() const;
-    void displayLog(ostream& out) const;
+
+    /**
+     * @brief Displays the transaction log to a specified output stream.
+     * 
+     * @param out The output stream to display the log (e.g., cout, ofstream).
+     * @param useColor If true, display the log with color formatting.
+     */
+    void displayLog(ostream& out, bool useColor) const;
 };
